a2ca4e898201800bbb9e09cb652f6212f9a3ba87
Rafael Martins <rafael.rmf2020@gmail.com>
2026-05-13 17:05:07 -0300

    ADD: Inputs Principais

---
+++ calculadora_viagem.py
@@ -0,0 +1,9 @@
+def calculadora_viagem():
+    orcamento_disponivel = float(input("Digite o orçamento disponível para a viagem: "))
+    destino_viagem = (input("Digite o destino da viagem: "))
+    custo_passagem = float(input("Digite o custo estimado da passagem aérea: "))
+    custo_diario = float(input("Digite o custo diário estimado para a viagem: "))
+    duracao_viagem = int(input("Digite a duração da viagem em dias: "))
+
+
+calculadora_viagem()
